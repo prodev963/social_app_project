b900eb92ff1e9a030122ffd498fc01aae4377493
shakhboz <l369hsh@gmail.com>
2022-07-18 17:03:04 +0500

    more on accounts(model,form,url)

--- social_project/accounts/admin.py
+++ social_project/accounts/admin.py
@@ -1,3 +1,6 @@
+import django
 from django.contrib import admin
-
+from .models import User
 # Register your models here.
+
+admin.site.register(User)
--- social_project/accounts/views.py
+++ social_project/accounts/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from django.urls import reverse_lazy
+from django.views.generic import FormView,ListView,CreateView,UpdateView,DeleteView,DetailView,TemplateView
+from . import models,forms
 # Create your views here.
+
+class SignUp(CreateView):
+    form_class = forms.UserCreateForm
+    success_url = reverse_lazy('login')
+    template_name: str = 'accounts/signup.html '
