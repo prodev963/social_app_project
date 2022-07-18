d6537b5b00b80b744d8845be24243eaafad2793b
shakhboz <l369hsh@gmail.com>
2022-07-18 23:56:59 +0500

    login-logout

--- social_project/social_project/settings.py
+++ social_project/social_project/settings.py
@@ -119,6 +119,10 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_DIR = Path(BASE_DIR/"static")
 
+
+LOGIN_REDIRECT_URL = 'test'
+LOGOUT_REDIRECT_URL = 'thanks'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
